a68818516c083bd6d3e19dc258f19b71654504a5
protosonic <ndebruin29@gmail.com>
2025-05-09 10:12:15 -0600

    Create Spring Boot MVC project, configure JPA dependency

---
+++ MealPlan/src/main/java/com/skilldistillery/mealplan/MealPlanApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.mealplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealPlanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealPlanApplication.class, args);
+	}
+
+}
---
+++ MealPlan/src/test/java/com/skilldistillery/mealplan/MealPlanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.mealplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealPlanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
